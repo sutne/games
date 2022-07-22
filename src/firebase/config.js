3e2d64bba401b912874869385abc0f634644b752
sutne <sivertutne@gmail.com>
2022-07-22 13:04:55 +0200

    init

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8VQWMlNUwi3UINJav7A-rKOtybP32Ey0",
+  authDomain: "sutne-games.firebaseapp.com",
+  projectId: "sutne-games",
+  storageBucket: "sutne-games.appspot.com",
+  messagingSenderId: "1006446422937",
+  appId: "1:1006446422937:web:45b42fab3341dd7add4108",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
