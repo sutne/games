6a609772eaa6d8b1e180c8632eccbe66e3a678c5
sutne <sivertutne@gmail.com>
2022-07-26 11:35:41 +0200

    move hardcoded colors to themes

--- src/themes.jsx
+++ src/themes.jsx
@@ -3,15 +3,21 @@ import { createTheme } from "@material-ui/core";
 export const darkTheme = createTheme({
   palette: {
     mode: "dark",
-    background: "rgb(13, 17, 23)",
-    text: "rgb(255, 255, 255)",
+    myBackground: "rgb(13, 17, 23)",
+    myText: "rgb(255, 255, 255)",
+    myCard: "rgb(40, 50, 65)",
+    myWrapperBackground: "rgb(19, 23, 35)",
+    myWrapperBorder: "rgb(27, 29, 42)",
   },
 });
 
 export const lightTheme = createTheme({
   palette: {
     mode: "light",
-    background: "rgb(255, 255, 255)",
-    text: "rgb(13, 17, 23)",
+    myBackground: "rgb(255, 255, 255)",
+    myText: "rgb(13, 17, 23)",
+    myCard: "rgb(255, 255, 255)",
+    myWrapperBackground: "rgb(240, 240, 240)",
+    myWrapperBorder: "rgb(230, 230, 230)",
   },
 });
