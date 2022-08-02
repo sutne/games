36282d2d2a62baedc94749ca7e18ca0f8b352542
sutne <sivertutne@gmail.com>
2022-08-02 16:12:46 +0200

    refactor

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -6,34 +6,40 @@ import {
   updateDoc,
   deleteDoc,
   DocumentSnapshot,
+  DocumentReference,
 } from "firebase/firestore";
 
-import { FirestoreDocument } from "./models/firestoreDocument";
+export async function createDocument(
+  collectionName: string,
+  data: { [key: string]: any }
+) {
+  return await addDoc(collection(firestore, collectionName), data);
+}
+
+export async function updateDocument(
+  data: { [key: string]: any },
+  ref?: DocumentReference
+) {
+  if (ref === undefined)
+    throw new Error("Cannot update document without a reference");
+  return await updateDoc(ref, data);
+}
+
+export async function deleteDocument(ref?: DocumentReference) {
+  if (ref === undefined)
+    throw new Error("Cannot delete document without a reference");
+  return await deleteDoc(ref);
+}
 
 export async function getCollection(
   name: string,
   converter: Function
-): Promise<FirestoreDocument[]> {
+): Promise<{ [key: string]: any }[]> {
   const querySnapshot = await getDocs(collection(firestore, name));
-  let documents: FirestoreDocument[] = [];
+  let documents: { [key: string]: any }[] = [];
   querySnapshot.forEach((doc: DocumentSnapshot) => {
     console.log(doc.id, " => ", doc.data());
     documents.push(converter(doc));
   });
   return documents;
-}
-
-export async function createDocument(document: FirestoreDocument) {
-  const collectionName = Object.getPrototypeOf(document).collectionName;
-  console.log("collectionName: ", collectionName);
-  const theCollection = collection(firestore, collectionName);
-  return await addDoc(theCollection, document.toFirestore());
-}
-
-export async function updateDocument(document: FirestoreDocument) {
-  return await updateDoc(document.ref, document.toFirestore());
-}
-
-export async function deleteDocument(document: FirestoreDocument) {
-  return await deleteDoc(document.ref);
 }
--- src/services/models/minesweeperDocument.ts
+++ src/services/models/minesweeperDocument.ts
@@ -1,47 +1,51 @@
 import { DocumentReference, DocumentSnapshot } from "firebase/firestore";
-import { FirestoreDocument } from "./firestoreDocument";
-import { Difficulty } from "../../pages/minesweeper/logic";
+import { Difficulty } from "pages/minesweeper/logic";
+import {
+  createDocument,
+  deleteDocument,
+  updateDocument,
+} from "services/firestore";
 
-export class MinesweeperDocument extends FirestoreDocument {
-  collectionName = "minesweeper";
-
-  user: string;
+type Fields = {
   time: number;
-  cleared: number;
+  score: number;
   difficulty: Difficulty;
+  user?: string;
+  ref?: DocumentReference;
+};
 
-  constructor(
-    ref: DocumentReference,
-    user: string,
-    time: number,
-    cleared: number,
-    difficulty: Difficulty
-  ) {
-    super(ref);
-    this.user = user;
-    this.time = time;
-    this.cleared = cleared;
-    this.difficulty = difficulty;
+export class MinesweeperDocument {
+  fields: Fields;
+  reference?: DocumentReference;
+
+  constructor(fields: Fields, reference?: DocumentReference) {
+    this.fields = fields;
+    this.reference = reference;
   }
 
-  toFirestore() {
-    return {
-      user: this.user,
-      time: this.time,
-      cleared: this.cleared,
-      difficulty: this.difficulty,
-    };
+  create() {
+    createDocument("minesweeper", this.fields);
+  }
+
+  update() {
+    updateDocument(this.fields, this.reference);
+  }
+
+  delete() {
+    deleteDocument(this.reference);
   }
 
   static fromFirestore(snapshot: DocumentSnapshot) {
     const data = snapshot.data();
     if (!data) return undefined;
     return new MinesweeperDocument(
-      snapshot.ref,
-      data.user,
-      data.time,
-      data.cleared,
-      data.difficulty
+      {
+        time: data.time,
+        score: data.score,
+        difficulty: data.difficulty,
+        user: data.user,
+      },
+      snapshot.ref
     );
   }
 }
