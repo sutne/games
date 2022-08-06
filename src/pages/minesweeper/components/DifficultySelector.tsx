8194af0dcb871aa98e745f8b729063fbc3204ff3
sutne <sivertutne@gmail.com>
2022-08-06 15:18:51 +0200

    Refactor provider

--- src/pages/minesweeper/components/DifficultySelector.tsx
+++ src/pages/minesweeper/components/DifficultySelector.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { Box, Button, Typography } from "@mui/material";
 
-import { Difficulty } from "../logic";
+import { useMinesweeper } from "../hooks/MinesweeperProvider";
+import { MinesweeperDifficulty } from "../minesweeper";
 
-export type DifficultyProps = {
-  setDifficulty: React.Dispatch<React.SetStateAction<Difficulty | undefined>>;
-};
-
-export function DifficultySelector({ setDifficulty }: DifficultyProps) {
+export function DifficultySelector() {
+  const { setDifficulty } = useMinesweeper();
   const classes = getClasses();
   return (
     <Box sx={classes.container}>
       <Typography variant="h5" sx={classes.description}>
         Select Difficulty
       </Typography>
-      <Button onClick={() => setDifficulty(Difficulty.BEGINNER)}>
+      <Button onClick={() => setDifficulty(MinesweeperDifficulty.BEGINNER)}>
         Beginner
       </Button>
-      <Button onClick={() => setDifficulty(Difficulty.INTERMEDIATE)}>
+      <Button onClick={() => setDifficulty(MinesweeperDifficulty.INTERMEDIATE)}>
         Intermediate
       </Button>
-      <Button onClick={() => setDifficulty(Difficulty.EXPERT)}>Expert</Button>
+      <Button onClick={() => setDifficulty(MinesweeperDifficulty.EXPERT)}>
+        Expert
+      </Button>
     </Box>
   );
 
---
+++ src/pages/minesweeper/hooks/MinesweeperProvider.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+
+import { getUserID, isSignedIn } from "services/firebase/auth";
+import { MinesweeperDocument } from "services/firebase/models/minesweeperDocument";
+
+import { Minesweeper, MinesweeperDifficulty } from "../minesweeper";
+
+const MinesweeperContext = React.createContext<
+  | {
+      game: Minesweeper | undefined;
+      updateGame: (callback: (game: Minesweeper) => void) => void;
+      setDifficulty: (difficulty: MinesweeperDifficulty) => void;
+      replay: () => void;
+      clear: () => void;
+    }
+  | undefined
+>(undefined);
+
+type props = {
+  children: JSX.Element;
+};
+export function MinesweeperProvider({ children }: props) {
+  const [game, setGame] = useState<Minesweeper>();
+
+  const replay = () => {
+    if (!game) return;
+    setGame(new Minesweeper(game.difficulty));
+  };
+
+  const setDifficulty = (difficulty: MinesweeperDifficulty) => {
+    setGame(new Minesweeper(difficulty));
+  };
+
+  const updateGame = (callback: (game: Minesweeper) => void) => {
+    if (!game) return;
+    const copy = game.copy();
+    callback(copy);
+    setGame(copy);
+  };
+
+  const clear = () => {
+    setGame(undefined);
+  };
+
+  // Save score and time to firestore once game is over
+  useEffect(() => {
+    if (!game) return;
+    if (!isSignedIn()) return;
+    if (!game.isOver()) return;
+    if (game.isSaved) return;
+    new MinesweeperDocument({
+      user: getUserID(),
+      time: game.stats.time,
+      clearPercentage: game.stats.clearPercentage,
+      correctFlags: game.stats.numCorrectFlags,
+      victory: game.isWon,
+      difficulty: game.difficulty,
+    }).create();
+    const copy = game.copy();
+    copy.isSaved = true;
+    setGame(copy);
+  }, [game]);
+
+  const contextContent = {
+    game,
+    updateGame,
+    setDifficulty,
+    replay,
+    clear,
+  };
+
+  return (
+    <MinesweeperContext.Provider value={contextContent}>
+      {children}
+    </MinesweeperContext.Provider>
+  );
+}
+
+export function useMinesweeper() {
+  const context = React.useContext(MinesweeperContext);
+  if (context === undefined) {
+    throw new Error("useMinesweeper must be used within a MinesweeperProvider");
+  }
+  return { ...context };
+}
