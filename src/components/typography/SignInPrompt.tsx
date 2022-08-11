25d3b551f46b6b61eb5fd2d4f1a1f41766fe60a3
sutne <sivertutne@gmail.com>
2022-08-11 19:42:50 +0200

    fix invalid div insice <p>

--- src/components/typography/SignInPrompt.tsx
+++ src/components/typography/SignInPrompt.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import { Link } from "components/interactive";
 
@@ -11,10 +11,10 @@ export function SignInPrompt({ pre, post }: props) {
   return (
     <>
       {pre ? <Typography key="pre">{pre}</Typography> : <></>}
-      <Typography key="prompt">
+      <Box key="prompt">
         <Link onClick={() => console.log("temp")}>Sign in</Link> or{" "}
         <Link onClick={() => console.log("temp")}>create a user</Link>
-      </Typography>
+      </Box>
       {post ? <Typography key="post">{post}</Typography> : <></>}
     </>
   );
