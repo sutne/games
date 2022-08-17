ec22a5b1777807f3f15114d66f2dfa0b5fe76e3e
sutne <sivertutne@gmail.com>
2022-08-17 17:27:46 +0200

    more readable

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -14,8 +14,7 @@ function convertTime(time: number): [string, string, string, string] {
 
 export function timeString(time: number) {
   const [h, m, s, hs] = convertTime(time);
-  // if (h === "00" && m === "00") return `${s}.${hs}`;
-  if (h === "00") return `${m}:${s}.${hs}`;
-  // if (m === "00") return `${s}.${hs}`;
-  return `${h}:${m}:${s}.${hs}`;
+  if (h === "00" && m === "00") return `${s}.${hs}s`;
+  if (h === "00") return `${m}:${s}`;
+  return `${h}:${m}:${s}`;
 }
